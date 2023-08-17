380489096af2597f440ac33e96d5142f4d36feb4
3braheem <ibraahimsiri@gmail.com>
2023-08-17 14:17:31 -0700

    Responsive Navbar

---
+++ components/HamburgerMenu.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { RowsIcon } from "@radix-ui/react-icons";
+import * as Dropdown from "@radix-ui/react-dropdown-menu";
+import Link from "next/link";
+
+const HamburgerMenu: React.FC = () => {
+  return (
+    <>
+      <Dropdown.Root>
+        <Dropdown.Trigger asChild>
+          <button aria-label="Navigate Pages" className="focus:outline-none">
+            <RowsIcon />
+          </button>
+        </Dropdown.Trigger>
+
+        <Dropdown.Portal>
+          <Dropdown.Content
+            sideOffset={2}
+            className="w-36 rounded m-4 py-2 border border-gray-800"
+          >
+            <Dropdown.Item className="flex items-center rounded mx-[1px] pl-4 h-10 text-lg outline-none hover:bg-yellow-400 hover:text-black">
+              <Link href="/about">About</Link>
+            </Dropdown.Item>
+            <Dropdown.Item className="flex items-center rounded mx-[1px] pl-4 h-10 text-lg outline-none hover:bg-yellow-400 hover:text-black">
+              <Link href="/etc">Etc.</Link>
+            </Dropdown.Item>
+          </Dropdown.Content>
+        </Dropdown.Portal>
+      </Dropdown.Root>
+    </>
+  );
+};
+
+export default HamburgerMenu;
---
+++ components/Navbar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import HamburgerMenu from "./HamburgerMenu";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="flex justify-between items-center py-10 px-12">
+      <h1 className="font-display text-3xl">DEMO</h1>
+      <div className="max-md:hidden">
+        <Link href="/about" className="hover:text-yellow-400 ml-4">
+          About
+        </Link>
+        <Link href="/etc" className="hover:text-yellow-400 ml-4">
+          Etc.
+        </Link>
+      </div>
+      <div className="md:hidden">
+        <HamburgerMenu />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
